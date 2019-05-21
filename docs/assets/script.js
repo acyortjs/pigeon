df3295528fe4c03c0fe565b526d7d09416995ed5
LoeiFy <LoeiFy@gmail.com>
2019-05-21 20:46:02 +0800

    history state

--- docs/assets/script.js
+++ docs/assets/script.js
@@ -15,6 +15,26 @@ class Query {
     return this
   }
 
+  html(content) {
+    if (content !== undefined) {
+      this.dom.innerHTML = content
+      return this
+    }
+    return this.dom.innerHTML
+  }
+
+  attr(attribute, value) {
+    if (!value) {
+      return this.dom.getAttribute(attribute)
+    }
+    this.dom.setAttribute(attribute, value)
+    return this
+  }
+
+  removeAttr(attribute) {
+    this.dom.removeAttribute(attribute)
+  }
+
   toggleClass(name) {
     this.dom.classList.toggle(name)
   }
@@ -34,13 +54,86 @@ class Query {
 
 const $ = dom => new Query(dom)
 
+const checkMenu = () => {
+  if (window.location.pathname !== '/') {
+    $('.menu > a').css('display', 'block')
+  } else {
+    $('.menu > a').css('display', 'none')
+  }
+}
+
+const posts = () => {
+  let loading = false
+  let prev
+
+  return (name, initiative = true) => {
+    if (loading || name === prev) {
+      return
+    }
+
+    prev = name
+    loading = true
+    $('.menu button').attr('disabled', true)
+
+    window.fetch(name)
+      .then(res => res.text())
+      .then((res) => {
+        const title = res
+          .split(/<title>([\s\S]*)<\/title>/)[1]
+          .replace(/[\r\n]/g, '')
+          .trim()
+        const main = res.match(/<body>([\s\S]*)<\/body>/)
+
+        const fragment = document.createElement('div');
+        [fragment.innerHTML] = main
+
+        const header = fragment.querySelector('.header')
+        const content = fragment.querySelector('.content')
+
+        document.title = title
+        $('.header').html(header.innerHTML)
+        $('.content').html(content.innerHTML)
+
+        if (initiative) {
+          window.history.pushState(null, title, name)
+        }
+
+        checkMenu()
+        loading = false
+        $('.menu button').removeAttr('disabled')
+      })
+      .catch(({ message }) => {
+        loading = false
+        prev = undefined
+        window.alert(message) // eslint-disable-line no-alert
+      })
+  }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   $('.menu > span').on('click', function click() {
     $(this).toggleClass('active')
     $('.menu > div').toggleClass('active')
   })
 
-  if (window.location.pathname !== '/') {
-    $('.menu > a').css('display', 'block')
-  }
+  checkMenu()
+
+  const getPost = posts()
+  $('body').on('click', (e) => {
+    const { tagName } = e.target
+    if (tagName !== 'A') {
+      return
+    }
+    const href = $(e.target).attr('href')
+    if (href.charAt() !== '/') {
+      return
+    }
+    e.preventDefault()
+    getPost(href)
+  })
+
+  window.addEventListener('popstate', () => {
+    const { pathname } = window.location
+    getPost(pathname, false)
+  })
 })
